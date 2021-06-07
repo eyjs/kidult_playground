8d7550f328f222abd7ec4a0bff161960640bbe0e
ljs <wnstn1342@gmail.com>
2021-06-07 19:37:14 +0900

    네이버 로그인 오류로 인한 디버깅  1. OAuthoAttributes Dto ofNaver() 메서드 오타    -> userNameAttributeName 객체에서 네이버 response get을 하지않고 attributes.get을 통한 버그 발생.    -> 해결 완료.

--- src/main/java/com/kidult_playground/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/kidult_playground/config/auth/CustomOAuth2UserService.java
@@ -75,13 +75,11 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user =
-                userRepository.findByEmail(
-                        attributes.getEmail())
-                            .map(entity -> entity.update(
-                                    attributes.getName(), attributes.getPicture()))
+                userRepository.findByEmail(attributes.getEmail())
+                            .map(entity ->
+                                    entity.update(attributes.getName(), attributes.getPicture()))
                                     .orElse(attributes.toEntity());
 
         return userRepository.save(user);
-
     }
 }
--- src/main/java/com/kidult_playground/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kidult_playground/config/auth/dto/OAuthAttributes.java
@@ -42,7 +42,7 @@ public class OAuthAttributes {
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
         if("naver".equals(registrationId)) {
-            return ofNaver(userNameAttributeName, attributes);
+            return ofNaver("id", attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -58,15 +58,14 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public static OAuthAttributes ofNaver(String userNameAttributeName,
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
                                           Map<String, Object> attributes){
-        Map<String, Object> response =
-                (Map<String, Object>) attributes.get("response");
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("profile_image"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
